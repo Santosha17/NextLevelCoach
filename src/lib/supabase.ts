600bf8ed71422a20eaa4d381038a3d0cb1134b83
JoãoSantos497 <210100497@esg.ipsantarem.pt>
2026-02-02 22:02:01 +0000

    update supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,7 +1,20 @@
-import { createBrowserClient } from '@supabase/ssr';
+import { createBrowserClient } from '@supabase/ssr'
 
-export const createClient = () =>
-    createBrowserClient(
+// Variável global para guardar a conexão
+let clientInstance: ReturnType<typeof createBrowserClient> | null = null;
+
+export function createClient() {
+    // Se a conexão já existe, DEVOLVE A MESMA (Singleton)
+    // Isto impede que o useEffect dispare novamente porque o objeto é exatamente o mesmo
+    if (clientInstance) {
+        return clientInstance;
+    }
+
+    // Se não existe, cria a primeira vez
+    clientInstance = createBrowserClient(
         process.env.NEXT_PUBLIC_SUPABASE_URL!,
         process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
     );
+
+    return clientInstance;
+}
