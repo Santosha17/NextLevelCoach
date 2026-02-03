d704de4ca0ab84190ad56c672bb3b569d090f7fc
JoãoSantos497 <210100497@esg.ipsantarem.pt>
2026-02-03 00:27:28 +0000

    updates

--- next.config.ts
+++ next.config.ts
@@ -1,18 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-    output: 'export', // Cria a pasta 'out' para o Cloudflare Pages
-
     images: {
-        // Obrigatório para Static Exports (como Cloudflare Pages)
-        // porque não há servidor para otimizar imagens em tempo real.
         unoptimized: true,
 
-        // Permite carregar imagens alojadas no Supabase
         remotePatterns: [
             {
                 protocol: 'https',
-                hostname: '**.supabase.co', // O asterisco ** aceita qualquer projeto Supabase
+                hostname: '**.supabase.co',
             },
         ],
     },
